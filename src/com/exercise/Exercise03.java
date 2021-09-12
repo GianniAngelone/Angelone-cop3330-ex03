dc26dfcbb2834b694d67b2dff4dd73a0abcf67c9
GianniAngelone <angelone.gianni@gmail.com>
2021-09-12 17:52:16 -0400

    Initial commit

---
+++ src/com/exercise/Exercise03.java
@@ -0,0 +1,21 @@
+ /*
+  *  UCF COP3330 Fall 2021 Assignment 2 Solution
+  *  Copyright 2021 first_name last_name
+  */
+package com.exercise;
+import java.util.Scanner;
+
+public class Exercise03 {
+
+     public static void main(String[] args)
+     {
+         Scanner input = new Scanner(System.in);
+         System.out.print("What is the quote? ");
+         String quote = input.nextLine();
+         System.out.print("Who said it? ");
+         String name = input.nextLine();
+         System.out.println(String.format("%s says, \"%s\" ", name, quote));
+
+     }
+ }
+
